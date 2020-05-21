d5a4c6954d33ef43b9450c792de0dfff27d78973
lloda <lloda@sarc.name>
2020-05-21 12:49:56 +0200

    Testing on gcc 8/9/10

--- config/ra.py
+++ config/ra.py
@@ -17,6 +17,13 @@ class Style: BRIGHT = '\x1b[1m'; RESET_ALL = '\x1b[0m';
 from os.path import join, abspath, split
 from subprocess import call
 
+CXXFLAGS = ['-std=c++17', '-Wall', '-Werror', '-fdiagnostics-color=always', '-Wno-unknown-pragmas',
+            '-finput-charset=UTF-8', '-fextended-identifiers',
+            '-Wno-error=strict-overflow', '-Werror=zero-as-null-pointer-constant',
+            #'-Wconversion',
+            # '-funsafe-math-optimizations', # TODO Test with this.
+        ]
+
 def ensure_ext(s, ext):
     "if s doesn't end with ext, append it."
     p = s.rfind(ext)
