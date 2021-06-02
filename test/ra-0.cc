aaa1d81458acf83d7aaf9044057e3e12e15857c0
lloda <lloda@sarc.name>
2021-06-02 12:36:54 +0200

    Remove use of deprecated std::is_literal_type

--- test/ra-0.cc
+++ test/ra-0.cc
@@ -56,16 +56,6 @@ void CheckArrayIO(TestRecorder & tr, A const & a, double * begin)
 int main()
 {
     TestRecorder tr(std::cout);
-    tr.section("concepts");
-    {
-        ra::Small<int, 2> a(0.), b(0.);
-        auto e0 = ra::expr([](int a, int b) { return a+b; }, start(a), start(b));
-        static_assert(std::is_literal_type_v<decltype(e0)>);
-        auto e1 = ra::expr([](int a, int b) { return a+b; }, start(a), ra::scalar(0.));
-        static_assert(std::is_literal_type_v<decltype(e1)>);
-        auto e2 = ra::expr([](int a, int b) { return a+b; }, start(a), ra::iota(2));
-        static_assert(std::is_literal_type_v<decltype(e2)>);
-    }
     tr.section("internal fields");
     {
         {
