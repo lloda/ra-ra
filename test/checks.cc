lloda/ra-ra

commit 32cf6930d9a155201e7579d4cd592f1cdac77422

Simplify prefix match checkers
* ra/core.hh: Merge atom.hh and match.hh.
* ra/ply.hh: Merge len.hh and ply.hh.
* ra/core.hh (Match::check_expr, Match::check_expr_s): Rewrite of
  ra::check_expr, ra::check_expr_s, now removed.
* ra/expr.hh (agree_s): New function.
* test/check.cc: Adapt checks of check_expr, check_expr_s.

ra/core.hh
@@ -190,8 +190,8 @@ struct Scalar
 
     constexpr static rank_t rank_s() { return 0; }
     constexpr static rank_t rank() { return 0; }
-    constexpr static dim_t len_s(int k) { RA_CHECK(k<0, "Bad axis ", k); std::abort(); }
-    constexpr static dim_t len(int k) { RA_CHECK(k<0, "Bad axis ", k); std::abort(); }
+    constexpr static dim_t len_s(int k) { std::abort(); }
+    constexpr static dim_t len(int k) { std::abort(); }
 
     constexpr static void adv(rank_t k, dim_t d) {}
     constexpr static dim_t step(int k) { return 0; }
@@ -341,8 +341,7 @@ struct Len
 
 constexpr Len len {};
 
-// let operators build expr trees.
-static_assert(IteratorConcept<Len>);
+// don't try to reduce operations with Len.
 template <> constexpr bool is_special_def<Len> = true;
 RA_IS_DEF(has_len, false);
 
@@ -413,5 +412,164 @@ FLAT(A && a)
 
 // FIXME do we really want to drop const? See use in concrete_type.
 template <class A> using value_t = std::decay_t<decltype(FLAT(std::declval<A>()))>;
+
+
+// --------------------
+// prefix match
+// --------------------
+
+constexpr rank_t
+choose_rank(rank_t ra, rank_t rb)
+{
+    return RANK_BAD==rb ? ra : RANK_BAD==ra ? rb : RANK_ANY==ra ? ra : RANK_ANY==rb ? rb : (ra>=rb ? ra : rb);
+}
+
+// TODO Allow infinite rank; need a special value of crank.
+constexpr rank_t
+dependent_cell_rank(rank_t rank, rank_t crank)
+{
+    return crank>=0 ? crank // not dependent
+        : rank==RANK_ANY ? RANK_ANY // defer
+        : (rank+crank);
+}
+
+constexpr rank_t
+dependent_frame_rank(rank_t rank, rank_t crank)
+{
+    return rank==RANK_ANY ? RANK_ANY // defer
+        : crank>=0 ? (rank-crank) // not dependent
+        : -crank;
+}
+
+constexpr dim_t
+choose_len(dim_t sa, dim_t sb)
+{
+    return DIM_BAD==sa ? sb : DIM_BAD==sb ? sa : DIM_ANY==sa ? sb : sa;
+}
+
+template <bool check, class T, class K=mp::iota<mp::len<T>>> struct Match;
+
+template <bool check, IteratorConcept ... P, int ... I>
+struct Match<check, std::tuple<P ...>, mp::int_list<I ...>>
+{
+    using T = std::tuple<P ...>;
+    T t;
+
+    constexpr static bool
+    check_expr_s()
+    {
+        for (int k=0; k<rank_s(); ++k) {
+            dim_t ls = len_s(k);
+            if (!((k>=std::decay_t<P>::rank_s() || choose_len(std::decay_t<P>::len_s(k), ls) == ls) && ...)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    constexpr bool
+    check_expr() const
+    {
+        for (int k=0; k<rank(); ++k) {
+            dim_t ls = len(k);
+            if (!((k>=std::get<I>(t).rank() || choose_len(std::get<I>(t).len(k), ls) == ls) && ...)) {
+                RA_CHECK(!check, "Mismatched lengths [", (std::array { std::get<I>(t).len(k) ... }), "] on axis ", k, ".");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    constexpr
+    Match(P ... p_): t(std::forward<P>(p_) ...)
+    {
+// TODO Maybe on ply, would avoid the check flag make agree_xxx() unnecessary.
+        if constexpr (check && !(has_len<P> || ...)) {
+            static_assert(check_expr_s(), "Mismatched shapes in expression.");
+            check_expr();
+        }
+    }
+
+// rank of largest subexpr, so we look at all of them.
+    constexpr static rank_t
+    rank_s()
+    {
+        rank_t r = RANK_BAD;
+        return ((r = choose_rank(r, ra::rank_s<P>())), ...);
+    }
+
+    constexpr static rank_t
+    rank()
+    requires (DIM_ANY != Match::rank_s())
+    {
+        return rank_s();
+    }
+
+    constexpr rank_t
+    rank() const
+    requires (DIM_ANY == Match::rank_s())
+    {
+        rank_t r = RANK_BAD;
+        ((r = choose_rank(r, std::get<I>(t).rank())), ...);
+        assert(RANK_ANY!=r); // not at runtime
+        return r;
+    }
+
+// first positive size, if none first DIM_ANY, if none then DIM_BAD
+    constexpr static dim_t
+    len_s(int k)
+    {
+        auto f = [&k]<class A>(dim_t s) {
+            constexpr rank_t ar = A::rank_s();
+            return (ar<0 || k<ar) ? choose_len(s, A::len_s(k)) : s;
+        };
+        dim_t s = DIM_BAD; ((s>=0 ? s : s = f.template operator()<std::decay_t<P>>(s)), ...);
+        return s;
+    }
+
+    constexpr static dim_t
+    len(int k)
+    requires (requires (int kk) { P::len(kk); } && ...)
+    {
+        return len_s(k);
+    }
+
+    constexpr dim_t
+    len(int k) const
+    requires (!(requires (int kk) { P::len(kk); } && ...))
+    {
+        auto f = [&k](dim_t s, auto const & a) {
+            return k<a.rank() ? choose_len(s, a.len(k)) : s;
+        };
+        dim_t s = DIM_BAD; ((s>=0 ? s : s = f(s, std::get<I>(t))), ...);
+        assert(DIM_ANY!=s); // not at runtime
+        return s;
+    }
+
+    constexpr void
+    adv(rank_t k, dim_t d)
+    {
+        (std::get<I>(t).adv(k, d), ...);
+    }
+
+    constexpr auto
+    step(int i) const
+    {
+        return std::make_tuple(std::get<I>(t).step(i) ...);
+    }
+
+    constexpr bool
+    keep_step(dim_t st, int z, int j) const
+    requires (!(requires (dim_t d, rank_t i, rank_t j) { P::keep_step(d, i, j); } && ...))
+    {
+        return (std::get<I>(t).keep_step(st, z, j) && ...);
+    }
+    constexpr static bool
+    keep_step(dim_t st, int z, int j)
+    requires (requires (dim_t d, rank_t i, rank_t j) { P::keep_step(d, i, j); } && ...)
+    {
+        return (std::decay_t<P>::keep_step(st, z, j) && ...);
+    }
+};
 
 } // namespace ra

ra/expr.hh
@@ -8,7 +8,7 @@
 // later version.
 
 #pragma once
-#include "match.hh"
+#include "core.hh"
 #include <functional>
 
 namespace ra {
@@ -253,7 +253,7 @@ map(Op && op, A && ... a)
 
 
 // ---------------
-// explicit agreement checks. FIXME provide separate agree_s().
+// explicit agreement checks
 // ---------------
 
 template <class ... P>
@@ -261,6 +261,13 @@ constexpr bool
 agree(P && ... p)
 {
     return agree_(ra::start(std::forward<P>(p)) ...);
+}
+
+template <class ... P>
+constexpr bool
+agree_s(P && ... p)
+{
+    return agree_s_(ra::start(std::forward<P>(p)) ...);
 }
 
 template <class Op, class ... P>
@@ -274,7 +281,14 @@ template <class ... P>
 constexpr bool
 agree_(P && ... p)
 {
-    return check_expr<false>(Match<false, std::tuple<P ...>> { std::forward<P>(p) ... });
+    return (Match<false, std::tuple<P ...>> { std::forward<P>(p) ... }).check_expr();
+}
+
+template <class ... P>
+constexpr bool
+agree_s_(P && ... p)
+{
+    return Match<false, std::tuple<P ...>>::check_expr_s();
 }
 
 template <class Op, class ... P>

test/checks.cc
@@ -104,6 +104,12 @@ int main()
 #undef EXPR
     }
 // If the size of an expr is static, dynamic checks may still need to be run if any of the terms of the expr has dynamic size. This is checked in match.hh: check_expr_s().
+    tr.section("static mismatch");
+    {
+        ra::Small<int, 2, 2> a;
+        ra::Small<int, 3, 2> b;
+        static_assert(!agree_s(a, b));
+    }
     tr.section("dynamic terms in match, static rank");
     {
         int error = 0;
@@ -111,7 +117,8 @@ int main()
         try {
             ra::Small<int, 2> a {2, 3};
             ra::Big<int, 1> b({1}, 77);
-            tr.test_eq(1, ra::check_expr_s<decltype(a+b)>());
+            tr.test_eq(true, decltype(a+b)::check_expr_s());
+            tr.test(!agree(a, b));
             a = b;
         } catch (ra_error & e) {
             error = 1;
@@ -179,7 +186,8 @@ int main()
         int error = 0;
         string s;
         try {
-            tr.info("dynamic test is needed").test_eq(1, ra::check_expr_s<decltype(EXPR)>());
+            tr.info("dynamic test is needed").test(decltype(EXPR)::check_expr_s());
+            tr.test(!agree(a, b));
             ply_ravel(EXPR);
         } catch (ra_error & e) {
             error = 1;
@@ -197,7 +205,8 @@ int main()
         try {
             std::cout << "A: " << a.iter().len(0) << endl;
             std::cout << "B: " << b.iter().len(0) << endl;
-            tr.info("dynamic test is needed").test_eq(1, ra::check_expr_s<decltype(EXPR)>());
+            tr.info("dynamic test is needed").test(decltype(EXPR)::check_expr_s());
+            tr.test(!agree(a, b));
             ply_ravel(EXPR);
         } catch (ra_error & e) {
             error = 1;

docs/ra-ra.texi
@@ -12,7 +12,7 @@
 @c References to source [ma··] or [ma···] current last is 117.
 
 @set VERSION 21
-@set UPDATED 2023 July 8
+@set UPDATED 2023 July 10
 
 @copying
 @code{ra::} (version @value{VERSION}, updated @value{UPDATED})
@@ -206,11 +206,11 @@ In C++, a library is enough.
 @c @cindex J # TODO makeinfo can't handle an entry appearing more than once (it creates multiple entries in the index).
 Generality is achieved by removing arbitrary restrictions and by adopting the rank extension mechanism of J. @code{ra::} supports array operations with an arbitrary number of arguments. Any of the arguments in an array expression can be read from or written to. Arrays or array expressions can be of any rank. Slicing operations work for subscripts of any rank, as in APL. You can use your own types as array elements.
 
-Consistency is achieved by having a clear set of concepts and having the realizations of those concepts adhere to the concept as closely as possible. @code{ra::} offers a few different types of views and containers, but it should be possible to use them interchangeably whenever the properties that justify their existence are not involved. When this isn't possible, it's a bug. For example, it used to be the case that you couldn't create a higher rank iterator on a @code{SmallView}, even though you could do it on a @code{View}; this was a bug.
+Consistency is achieved by having a clear set of concepts and having the realizations of those concepts adhere to the concept as closely as possible. @code{ra::} offers a few different types of views and containers, but it should be possible to use them interchangeably whenever it makes sense. For example, it used to be the case that you couldn't create a higher rank iterator on a @code{SmallView}, even though you could do it on a @code{View}; this was a bug.
 
-Sometimes consistency requires a choice. For example, given array views A and B, @code{A=B} copies the contents of view B into view A. To change view A instead (to treat A as a pointer) would be the default meaning of A=B for C++ types, and result in better consistency with the rest of the language, but I have decided that having consistency between views and containers (which ‘are’ their contents in a sense that views aren't) is more important.@footnote{This particular decision may be revisited in the future.}
+Sometimes consistency requires a choice. For example, given array views A and B, @code{A=B} copies the contents of view @code{B} into view @code{A}. To change view @code{A} instead (to treat @code{A} as a pointer) would be the default meaning of @code{A=B} for C++ types, and result in better consistency with the rest of the language, but I have decided that having consistency between views and containers (which ‘are’ their contents in a sense that views aren't) is more important.
 
-Transparency is achieved by avoiding unnecessary abstraction. An array view consists of a pointer and a list of steps and I see no point in hiding that. Manipulating the steps directly is often useful. A container consists of storage and a view and that isn't hidden either. Some of the types have an obscure implementation but I consider that a defect. Ideally you should be able to rewrite expressions on the fly, or plug in your own traversal methods or storage handling.
+Transparency is achieved by avoiding unnecessary abstraction. An array view consists of a pointer and a list of steps and I see no point in hiding it. Manipulating the steps directly is often useful. A container consists of storage and a view and that isn't hidden either. Some of the types have an obscure implementation but I consider that a defect. Ideally you should be able to rewrite expressions on the fly, or plug in your own traversal methods or storage handling.
 
 That isn't to mean that you need to be in command of a lot of internal detail to be able to use the library. I hope to have provided a high level interface to most operations and a reasonably usable syntax. However, transparency is critical to achieve interoperation with external libraries and languages. When you need to, you'll be able to guarantee that an array is stored in compact columns, or that the real parts are interleaved with the imaginary parts.
 
@@ -1695,7 +1695,7 @@ Item 2
 @section Type hierarchy
 @c ------------------------------------------------
 
-Some of the categories below are actually C++20 ‘concepts’, some are still informal.
+Some of the categories below are C++20 ‘concepts’, some are still informal.
 
 @itemize
 @item @b{Container} --- @code{Big}, @code{Shared}, @code{Unique}, @code{Small}

ra/big.hh
@@ -1114,7 +1114,7 @@ auto explode(View<T, RANK> const & a)
     return explode_<super_t, (std::is_same_v<super_t, std::complex<T>> ? 1 : rank_s<super_t>())>(a);
 }
 
-// FIXME Consider these in as namespace level generics in atom.hh
+// FIXME Maybe namespace level generics in core.hh
 template <class T> inline int gstep(int i) { if constexpr (is_scalar<T>) return 1; else return T::step(i); }
 template <class T> inline int glen(int i) { if constexpr (is_scalar<T>) return 1; else return T::len(i); }
 

ra/len.hh
@@ -1,113 +1,0 @@
-// -*- mode: c++; coding: utf-8 -*-
-// ra-ra - Special object len.
-
-// (c) Daniel Llorens - 2023
-// This library is free software; you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 3 of the License, or (at your option) any
-// later version.
-
-#pragma once
-#include "pick.hh"
-#include "expr.hh"
-
-namespace ra {
-
-
-// ---------------------
-// does expr tree contain Len?
-// ---------------------
-
-template <>
-constexpr bool has_len_def<Len> = true;
-
-template <IteratorConcept ... P>
-constexpr bool has_len_def<Pick<std::tuple<P ...>>> = (has_len<P> || ...);
-
-template <class Op, IteratorConcept ... P>
-constexpr bool has_len_def<Expr<Op, std::tuple<P ...>>> = (has_len<P> || ...);
-
-template <int w, class O, class N, class S>
-constexpr bool has_len_def<Iota<w, O, N, S>> = (has_len<O> || has_len<N> || has_len<S>);
-
-
-// ---------------------
-// replace Len in expr tree.
-// ---------------------
-
-template <class E_>
-struct WithLen
-{
-// constant & scalar appear in Iota args. dots_t and insert_t appear in subscripts.
-// FIXME what else? restrict to IteratorConcept<E_> || is_constant<E_> || is_scalar<E_> ...
-    template <class E> constexpr static decltype(auto)
-    f(dim_t len, E && e)
-    {
-        return std::forward<E>(e);
-    }
-};
-
-template <>
-struct WithLen<Len>
-{
-    template <class E> constexpr static decltype(auto)
-    f(dim_t len, E && e)
-    {
-        return Scalar<dim_t>(len);
-    }
-};
-
-template <class Op, IteratorConcept ... P, int ... I>
-requires (has_len<P> || ...)
-struct WithLen<Expr<Op, std::tuple<P ...>, mp::int_list<I ...>>>
-{
-    template <class E> constexpr static decltype(auto)
-    f(dim_t len, E && e)
-    {
-        return expr(std::forward<E>(e).op, WithLen<std::decay_t<P>>::f(len, std::get<I>(std::forward<E>(e).t)) ...);
-    }
-};
-
-template <IteratorConcept ... P, int ... I>
-requires (has_len<P> || ...)
-struct WithLen<Pick<std::tuple<P ...>, mp::int_list<I ...>>>
-{
-    template <class E> constexpr static decltype(auto)
-    f(dim_t len, E && e)
-    {
-        return pick(WithLen<std::decay_t<P>>::f(len, std::get<I>(std::forward<E>(e).t)) ...);
-    }
-};
-
-template <int w, class O, class N, class S>
-requires (has_len<O> || has_len<N> || has_len<S>)
-struct WithLen<Iota<w, O, N, S>>
-{
-// usable iota types must be either is_constant or is_scalar.
-    template <class T> constexpr static decltype(auto)
-    coerce(T && t)
-    {
-        if constexpr (IteratorConcept<T>) {
-            return FLAT(t);
-        } else {
-            return std::forward<T>(t);
-        }
-    }
-
-    template <class E> constexpr static decltype(auto)
-    f(dim_t len, E && e)
-    {
-        return iota<w>(coerce(WithLen<std::decay_t<N>>::f(len, std::forward<E>(e).n)),
-                       coerce(WithLen<std::decay_t<O>>::f(len, std::forward<E>(e).i)),
-                       coerce(WithLen<std::decay_t<S>>::f(len, std::forward<E>(e).s)));
-    }
-};
-
-template <class E>
-constexpr decltype(auto)
-with_len(dim_t len, E && e)
-{
-    return WithLen<std::decay_t<E>>::f(len, std::forward<E>(e));
-}
-
-} // namespace ra

commit b90ab82a9c1f267ced89841a5f78d132422e38e9

Tighten match checks
Fix an issue introduced in 32cf6930d9a155201e7579d4cd592f1cdac77422 which
resulted in unnecessary runtime match checks.

* src/expr.hh (Match::check_expr_s): Return fail/rt/pass instead of
  false/true. Always pass when there are <2 subexpressions.
  (check_expr): If check_expr_s is pass, skip runtime checks.
  (Match): Wrap check_expr in RA_CHECK.
* test/checks.cc: Add a couple tests.

ra/core.hh
@@ -460,33 +460,47 @@ struct Match<check, std::tuple<P ...>, mp::int_list<I ...>>
     using T = std::tuple<P ...>;
     T t;
 
-    constexpr static bool
+    // 0: fail, 1: check rt, 2: pass
+    constexpr static int
     check_expr_s()
     {
-        for (int k=0; k<rank_s(); ++k) {
-            dim_t ls = len_s(k);
-            if (!((k>=std::decay_t<P>::rank_s() || choose_len(std::decay_t<P>::len_s(k), ls) == ls) && ...)) {
-                return false;
+        if constexpr (sizeof...(P)<2) {
+            return 2;
+        } else if constexpr (RANK_ANY==rank_s()) {
+            return 1; // FIXME could be tightened to 2 in some cases
+        } else {
+            bool tbc = false;
+            for (int k=0; k<rank_s(); ++k) {
+                dim_t ls = len_s(k);
+                if (!((k>=std::decay_t<P>::rank_s() || ls==choose_len(std::decay_t<P>::len_s(k), ls)) && ...)) {
+                    return 0;
+                } else {
+                    int anyk = ((k<std::decay_t<P>::rank_s() && (DIM_ANY==std::decay_t<P>::len_s(k))) + ...);
+                    int fixk = ((k<std::decay_t<P>::rank_s() && (0<=std::decay_t<P>::len_s(k))) + ...);
+                    tbc = tbc || (anyk>0 && anyk+fixk>1);
+                }
             }
+            return tbc ? 1 : 2;
         }
-        return true;
     }
 
     constexpr bool
     check_expr() const
     {
-        if constexpr (check_expr_s()) {
+        if constexpr (sizeof...(P)<2) {
+            return true;
+        } else  if constexpr (constexpr int c = check_expr_s(); 0==c) {
+            return false;
+        } else if constexpr (1==c) {
             for (int k=0; k<rank(); ++k) {
                 dim_t ls = len(k);
-                if (!((k>=std::get<I>(t).rank() || choose_len(std::get<I>(t).len(k), ls) == ls) && ...)) {
+                if (!((k>=std::get<I>(t).rank() || ls==choose_len(std::get<I>(t).len(k), ls)) && ...)) {
                     RA_CHECK(!check, "Mismatched lengths [", (std::array { std::get<I>(t).len(k) ... }), "] on axis ", k, ".");
                     return false;
                 }
             }
-            return true;
-        } else {
-            return false;
         }
+        return true;
     }
 
     constexpr
@@ -495,7 +509,7 @@ struct Match<check, std::tuple<P ...>, mp::int_list<I ...>>
 // TODO Maybe on ply, would avoid the check flag make agree_xxx() unnecessary.
         if constexpr (check && !(has_len<P> || ...)) {
             static_assert(check_expr_s(), "Mismatched shapes in expression.");
-            check_expr();
+            RA_CHECK(check_expr());
         }
     }
 
@@ -504,7 +518,7 @@ struct Match<check, std::tuple<P ...>, mp::int_list<I ...>>
     rank_s()
     {
         rank_t r = RANK_BAD;
-        return ((r = choose_rank(r, ra::rank_s<P>())), ...);
+        return ((r=choose_rank(r, ra::rank_s<P>())), ...);
     }
 
     constexpr static rank_t

ra/expr.hh
@@ -263,8 +263,9 @@ agree(P && ... p)
     return agree_(ra::start(std::forward<P>(p)) ...);
 }
 
+// 0: fail, 1: check rt, 2: pass
 template <class ... P>
-constexpr bool
+constexpr int
 agree_s(P && ... p)
 {
     return agree_s_(ra::start(std::forward<P>(p)) ...);
@@ -285,7 +286,7 @@ agree_(P && ... p)
 }
 
 template <class ... P>
-constexpr bool
+constexpr int
 agree_s_(P && ... p)
 {
     return Match<false, std::tuple<P ...>>::check_expr_s();

test/checks.cc
@@ -93,6 +93,8 @@ int main()
     {
         ra::Big<int, 3> a({2, 3, 4}, (ra::_0+1)*100 + (ra::_1+1)*10 + (ra::_2+1));
         ra::Big<int, 4> b({2, 4, 4, 5}, (ra::_0+1)*1000 + (ra::_1+1)*100 + (ra::_2+1)*10 + (ra::_3+1));
+        tr.test_eq(1, agree_s(a, b));
+        tr.test_eq(0, agree(a, b));
 #define EXPR expr([](auto && a, auto && b) { return a+b; }, start(a), start(b))
         int x = 0;
         try {
@@ -108,7 +110,19 @@ int main()
     {
         ra::Small<int, 2, 2> a;
         ra::Small<int, 3, 2> b;
-        static_assert(!agree_s(a, b));
+        static_assert(0==agree_s(a, b));
+    }
+    tr.section("static match");
+    {
+        ra::Small<int, 2, 2> a;
+        ra::Small<int, 2> b;
+        static_assert(2==agree_s(a, b));
+    }
+    tr.section("static match with dynamic term");
+    {
+        ra::Small<int> a;
+        ra::Big<int, 1> b({1}, 77);
+        tr.info("with rank ", decltype(a+b)::rank_s()).test_eq(2, agree_s(a, b));
     }
     tr.section("dynamic terms in match, static rank");
     {
@@ -117,7 +131,8 @@ int main()
         try {
             ra::Small<int, 2> a {2, 3};
             ra::Big<int, 1> b({1}, 77);
-            tr.test_eq(true, decltype(a+b)::check_expr_s());
+            std::cout << "LEN " << decltype(a+b)::len_s(0) << std::endl;
+            tr.info("with rank ", decltype(a+b)::rank_s()).test_eq(1, decltype(a+b)::check_expr_s());
             tr.test(!agree(a, b));
             a = b;
         } catch (ra_error & e) {
@@ -186,7 +201,7 @@ int main()
         int error = 0;
         string s;
         try {
-            tr.info("dynamic test is needed").test(decltype(EXPR)::check_expr_s());
+            tr.info("dynamic test is needed").test(1==decltype(EXPR)::check_expr_s());
             tr.test(!agree(a, b));
             ply_ravel(EXPR);
         } catch (ra_error & e) {
@@ -205,7 +220,7 @@ int main()
         try {
             std::cout << "A: " << a.iter().len(0) << endl;
             std::cout << "B: " << b.iter().len(0) << endl;
-            tr.info("dynamic test is needed").test(decltype(EXPR)::check_expr_s());
+            tr.info("dynamic test is needed").test(1==decltype(EXPR)::check_expr_s());
             tr.test(!agree(a, b));
             ply_ravel(EXPR);
         } catch (ra_error & e) {