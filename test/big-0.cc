lloda/ra-ra

commit 062a1fcecd7f26c88e3ad5a8270448ab15e4fc34

Use std::array for fixed rank ViewBig::dimv
* ra/arrays.hh (ViewBig): As stated. Fix constructors to deduce {{len, step}
  ...} like it was with dimv Small.
  (Container): Fix for std::array<T, N> not having have a fill constructor.
* ra/ra-0.cc: Rank 0 constructors like View({}, cp) are now ambiguous. Use
  View(cp) instead.
* docs/ra-ra.texi (Containers and views): Note ambiguity.

ra/arrays.hh
@@ -261,7 +261,7 @@ braces_shape(braces<T, R> const & l)
 template <class P, rank_t R>
 struct ViewBig
 {
-    using Dimv = std::conditional_t<ANY==R, vector_default_init<Dim>, Small<Dim, ANY==R ? 0 : R>>;
+    using Dimv = std::conditional_t<ANY==R, vector_default_init<Dim>, std::array<Dim, ANY==R ? 0 : R>>;
     [[no_unique_address]] Dimv dimv;
     P cp;
     using T = std::remove_reference_t<decltype(*cp)>;
@@ -276,22 +276,16 @@ struct ViewBig
     constexpr bool empty() const { return any(0==map(&Dim::len, dimv)); }
 
     constexpr ViewBig() {} // used by Container constructors
-    constexpr explicit ViewBig(P cp_): cp(cp_) {} // used by slicers, esp. when P has_len
+    constexpr explicit ViewBig(P cp_): cp(cp_) {} // empty dimv, but also uninit by slicers, esp. has_len<P>
     constexpr ViewBig(ViewBig const &) = default;
-    constexpr ViewBig(Dimv const & dimv_, P cp_): dimv(dimv_), cp(cp_) {} // [ra36]
-    constexpr ViewBig(auto const & s, P cp_): cp(cp_)
-    {
-        if constexpr (std::is_convertible_v<value_t<decltype(s)>, Dim>) {
-            ra::resize(dimv, ra::size(s)); // [ra37]
-            ra::iter(dimv) = s;
-        } else {
-            filldimv(ra::iter(s), dimv);
-        }
-    }
-    constexpr ViewBig(std::initializer_list<dim_t> s, P cp_): ViewBig(ra::iter(s), cp_) {}
+    constexpr ViewBig(auto const & s, P cp_) requires (requires { [](dim_t){}(VAL(s)); }): cp(cp_) { filldimv(ra::iter(s), dimv); }
+    constexpr ViewBig(auto const & s, P cp_): cp(cp_) { ra::resize(dimv, ra::size(s)); ra::iter(dimv) = s; } // [ra37]
+    template <class D> using dimbraces = std::conditional_t<R==ANY, std::initializer_list<D>, D (&&)[R==ANY?0:R]>;
+    constexpr ViewBig(dimbraces<dim_t> s, P cp_): ViewBig(ra::iter(s), cp_) {}
+    constexpr ViewBig(dimbraces<Dim> s, P cp_): ViewBig(ra::iter(s), cp_) {}
 // row-major ravel braces
     constexpr ViewBig const &
-    operator=(std::initializer_list<T> const x) const requires (1!=R)
+    operator=(std::initializer_list<T> x) const requires (1!=R)
     {
         auto xsize = ssize(x);
         RA_CK(size()==xsize, "Mismatched sizes ", size(), " ", xsize, ".");
@@ -299,17 +293,15 @@ struct ViewBig
         return *this;
     }
 // nested braces
-    constexpr ViewBig const &
-    operator=(braces<T, R> x) const requires (R!=ANY) { ra::iter<-1>(*this) = x; return *this; }
+    constexpr ViewBig const & operator=(braces<T, R> x) const requires (R!=ANY) { iter<-1>() = x; return *this; }
 #define RA_BRACES(N)                                                    \
-    constexpr ViewBig const &                                           \
-    operator=(braces<T, N> x) const requires (R==ANY) { ra::iter<-1>(*this) = x; return *this; }
+    constexpr ViewBig const & operator=(braces<T, N> x) const requires (R==ANY) { iter<-1>() = x; return *this; }
     RA_FE(RA_BRACES, 2, 3, 4);
 #undef RA_BRACES
 // T not is_scalar [ra44]
-    constexpr ViewBig const & operator=(T const & t) const { ra::iter(*this) = ra::scalar(t); return *this; }
+    constexpr ViewBig const & operator=(T const & t) const { iter() = ra::scalar(t); return *this; }
 // cf RA_ASSIGNOPS_ITER [ra38][ra34]
-    ViewBig const & operator=(ViewBig const & x) const { ra::iter(*this) = x; return *this; }
+    ViewBig const & operator=(ViewBig const & x) const { iter() = x; return *this; }
 #define RA_ASSIGNOPS(OP)                                                \
     constexpr ViewBig const & operator OP (auto const & x) const { ra::iter(*this) OP x; return *this; } \
     constexpr ViewBig const & operator OP (Iterator auto && x) const { ra::iter(*this) OP RA_FW(x); return *this; }
@@ -425,11 +417,12 @@ struct Container: public ViewBig<typename storage_traits<Store>::T *, R>
         store = storage_traits<Store>::create(filldimv(ra::iter(s), dimv));
         cp = storage_traits<Store>::data(store);
     }
-    constexpr void init(dim_t s) { init(std::array {s}); } // scalar allowed as shape if rank is 1.
+    constexpr void init(dim_t s) { init(std::array {s}); } // allow scalar if rank is 1.
 
 // provided so that {} calls sharg constructor below.
-    constexpr Container(): View({Dim {0, 1}}, nullptr) {}
-    constexpr Container() requires (0==R): Container({}, none) {}
+    constexpr static auto zdims = std::apply([](auto ... i){ return std::array<Dim, (R==ANY)?1:R>{Dim{i*0, 1} ... }; }, mp::iota<(R==ANY)?1:R>{});
+    constexpr Container(): View(zdims, nullptr) {}
+    constexpr Container() requires (R==0): Container({}, none) {}
 
     using sharg = decltype(shape(std::declval<View>().iter()));
 // sharg overloads handle {...} arguments. Size check is at conversion if sharg is Small.

docs/ra-ra.texi
@@ -486,6 +486,8 @@ Views can point to anywhere in memory and that memory doesn't have to belong to 
 int raw[6] = {1, 2, 3, 4, 5, 6};
 ra::ViewBig<int *> v1({{2, 3}, {3, 1}}, raw); // view with shape [2, 3] steps [3, 1]
 ra::ViewBig<int *> v2({2, 3}, raw);           // same, default C (row-major) steps
+// ra::ViewBig<int *> v2({}, raw);            // bogus error: ambiguous dimensions
+ra::ViewBig<int *> v2(raw);                   // do this instead for rank-0 view
 @end verbatim
 @end example
 

ra/base.hh
@@ -534,8 +534,7 @@ template <class A> constexpr auto fmt(format_t f, A && a) { return Fmt<A> { f, R
 RA_IS_DEF(is_array_formattable, is_ra<A> || (is_fov<A> && !std::is_convertible_v<A, std::string_view>));
 
 constexpr std::ostream & operator<<(std::ostream & o, is_array_formattable auto && a) { return o << fmt({}, RA_FW(a)); }
-template <class T>
-constexpr std::ostream & operator<<(std::ostream & o, std::initializer_list<T> const & a) { return o << fmt({}, a); }
+template <class T> constexpr std::ostream & operator<<(std::ostream & o, std::initializer_list<T> a) { return o << fmt({}, a); }
 
 constexpr std::ostream &
 operator<<(std::ostream & o, std::source_location const & loc)

test/checks.cc
@@ -347,6 +347,5 @@ int main()
         tr.info("caught error L" RA_STRINGIZE(__LINE__) ": ", s).test_eq(1, error);
     }
 #undef MAP
-
     return tr.summary();
 }

test/owned.cc
@@ -19,16 +19,15 @@ int main()
     TestRecorder tr;
     tr.section("resize first dimension");
     {
-        auto test = [&tr](auto const & ref, auto & a, int newsize, int testsize)
-            {
-                a.resize(newsize);
-                tr.test_eq(ref.rank(), a.rank());
-                tr.test_eq(newsize, a.len(0));
-                for (int i=1; i<a.rank(); ++i) {
-                    tr.test_eq(ref.len(i), a.len(i));
-                }
-                tr.test_eq(ref(ra::iota(testsize)), a(ra::iota(testsize)));
-            };
+        auto test = [&tr](auto const & ref, auto & a, int newsize, int testsize){
+            a.resize(newsize);
+            tr.test_eq(ref.rank(), a.rank());
+            tr.test_eq(newsize, a.len(0));
+            for (int i=1; i<a.rank(); ++i) {
+                tr.test_eq(ref.len(i), a.len(i));
+            }
+            tr.test_eq(ref(ra::iota(testsize)), a(ra::iota(testsize)));
+        };
         tr.section("rank 2");
         {
             ra::Big<int, 2> a({5, 3}, ra::_0 - ra::_1);
@@ -69,18 +68,17 @@ int main()
     tr.section("push back");
     {
         real check[] = { 2, 3, 4, 7 };
-        auto test = [&tr, &check](auto && z)
-            {
-                tr.test_eq(0, z.len(0));
-                tr.test_eq(1, z.step(0));
-                for (int i=0; i<4; ++i) {
-                    z.push_back(check[i]);
-                    tr.test_eq(i+1, z.size());
-                    for (int j=0; j<=i; ++j) {
-                        tr.test_eq(check[j], z[j]);
-                    }
+        auto test = [&tr, &check](auto && z){
+            tr.test_eq(0, z.len(0));
+            tr.test_eq(1, z.step(0));
+            for (int i=0; i<4; ++i) {
+                z.push_back(check[i]);
+                tr.test_eq(i+1, z.size());
+                for (int j=0; j<=i; ++j) {
+                    tr.test_eq(check[j], z[j]);
                 }
-            };
+            }
+        };
         test(ra::Big<real, 1>());
         ra::Big<real> z = ra::Big<real, 1>();
         test(z);

test/ra-0.cc
@@ -15,8 +15,8 @@
 
 using std::cout, std::endl, std::flush, ra::TestRecorder;
 
-template <class A>
-void CheckArrayOutput(TestRecorder & tr, A const & a, double * begin)
+void
+CheckArrayOutput(TestRecorder & tr, auto const & a, double * begin)
 {
     std::ostringstream o;
     o << a;
@@ -28,7 +28,8 @@ void CheckArrayOutput(TestRecorder & tr, A const & a, double * begin)
 }
 
 template <class A>
-void CheckArrayIO(TestRecorder & tr, A const & a, double * begin)
+void
+CheckArrayIO(TestRecorder & tr, A const & a, double * begin)
 {
     std::ostringstream o;
     o << a;
@@ -141,7 +142,7 @@ int main()
         auto rank0test0 = [](double & a) { a *= 2; };
         auto rank0test1 = [](double const & a) { return a*2; };
         double x { 99 };
-        ra::ViewBig<double *, 0> a { {}, &x };
+        ra::ViewBig<double *, 0> a (&x); // FIXME ({}, cp) is ambiguous; fixable here, but harder for var rank
         tr.test_eq(1, a.size());
 
 // ra::ViewBig<T *, 0> contains a pointer to T plus the dope vector of type Small<Dim, 0>. But after I put the data of Small in Small itself instead of in SmallBase, sizeof(Small<T, 0>) is no longer 0.
@@ -704,11 +705,27 @@ int main()
             double check[4] = { 3, 1, 2, 3 };
             CheckArrayOutput(tr, r, check);
         }
+        tr.section("6b");
+        // {
+        //     int data[] = { 1, 2, 3, 4 };
+        //     ra::ViewBig<int *, 2> a({2, 2, 2}, data); // fails at compile time [ra41] test
+        //     tr.test(sizeof(a)>0);
+        // }
         tr.section("7");
         {
             double rpool[1] = { 88 };
-            ra::ViewBig<double *, 0> r { {}, rpool };
+            ra::ViewBig<double *, 0> r (rpool); // FIXME ({}, cp) is ambiguous; fixable here, but harder for var rank
             double check[1] = { 88 };
+            CheckArrayOutput(tr, r, check);
+            tr.test_eq(1, r.size());
+            // static_assert(sizeof(r)==sizeof(double *), "bad assumption"); [170]
+            tr.test_eq(88, r);
+        }
+        tr.section("7b");
+        {
+            double rpool[1] = { 88 };
+            ra::ViewBig<double *> r (rpool); // FIXME ({}, cp) is ambiguous
+            double check[2] = { 0, 88 };
             CheckArrayOutput(tr, r, check);
             tr.test_eq(1, r.size());
             // static_assert(sizeof(r)==sizeof(double *), "bad assumption"); [170]
@@ -740,6 +757,8 @@ int main()
             std::iota(check+4, check+4+24, 0);
             CheckArrayIO(tr, a, check);
         }
+
+
     }
     tr.section("ply - xpr types - Scalar");
     {

test/ra-9.cc
@@ -1,7 +1,7 @@
 // -*- mode: c++; coding: utf-8 -*-
 // ra-ra/test - Regressions in array-in-ra::scalar.
 
-// (c) Daniel Llorens - 2017
+// (c) Daniel Llorens - 2017-2026
 // This library is free software; you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
 // Software Foundation; either version 3 of the License, or (at your option) any
@@ -89,19 +89,19 @@ int main()
     tr.section("ra::iter() on foreign types");
     {
         auto ref = std::array<int, 4> {12, 77, 44, 1};
-        tr.test_eq(2, ra::map_([](int i) { return i; }, ra::iter(std::vector {1, 2, 3})).at(ra::Small<int, 1>{1}));
-        tr.test_eq(ref, ra::map_([](int i) { return i; }, ra::iter(std::array {12, 77, 44, 1})));
+        tr.test_eq(2, ra::map_([](int i){ return i; }, ra::iter(std::vector {1, 2, 3})).at(ra::Small<int, 1>{1}));
+        tr.test_eq(ref, ra::map_([](int i){ return i; }, ra::iter(std::array {12, 77, 44, 1})));
 // [ra1] used to need forward in Match/Map/Pick bc CTE in older gcc. But see bug83.cc.
-        tr.test_eq(ref, ra::map_([](int i) { return i; }, ra::iter(ra::Big<int, 1> {12, 77, 44, 1})));
-        tr.test_eq(ref, ra::map_([](int i) { return i; }, ra::iter(std::vector {12, 77, 44, 1})));
+        tr.test_eq(ref, ra::map_([](int i){ return i; }, ra::iter(ra::Big<int, 1> {12, 77, 44, 1})));
+        tr.test_eq(ref, ra::map_([](int i){ return i; }, ra::iter(std::vector {12, 77, 44, 1})));
         {
             int c = 0;
-            ply_ravel(ra::map_([&c](int i) { c += i; }, ra::iter(ra::Unique<int, 1> {12, 77, 44, 1})));
+            ply_ravel(ra::map_([&c](int i){ c += i; }, ra::iter(ra::Unique<int, 1> {12, 77, 44, 1})));
             tr.test_eq(12+77+44+1, c);
         }
         {
             int c = 0;
-            for_each([&c](int i) { c += i; }, ra::Unique<int, 1> {12, 77, 44, 1});
+            for_each([&c](int i){ c += i; }, ra::Unique<int, 1> {12, 77, 44, 1});
             tr.test_eq(12+77+44+1, c);
         }
     }
@@ -111,7 +111,7 @@ int main()
         std::array a1 = {1, 2};
         std::vector a2 = {1, 2};
 
-        for_each([](auto && a, auto && b) { a = b; }, ra::ptr(a1), 99);
+        for_each([](auto && a, auto && b){ a = b; }, ra::ptr(a1), 99);
         tr.test_eq(99, ra::iter(a1));
     }
     tr.section("[ra35] - value");
@@ -132,9 +132,9 @@ int main()
         tr.test_eq(1, a1[0]);
         tr.test_eq(11, a1[1]);
     }
-// This used to be supported, but it really made no sense. Now v0 is a read-only location so this will ct error [ra42]
+// This used to work, but it made no sense. Now v0 is a read-only location so this will ct error [ra42]
     // {
-    //     auto fun1 = [](int a) { return std::array {a, a+10}; };
+    //     auto fun1 = [](int a){ return std::array {a, a+10}; };
     //     auto v0 = ra::ptr(fun1(0));
     //     auto v1 = ra::ptr(fun1(1));
     //     v0 = v1;

commit ff64b40a7db49a56b74414890ca8d11b491eb447

Forbid short dimension list in Container constructor
* ra/arrays.h (Container::Container): Take dims by C array with deduced length
  and not by std::array, because std::array allows short list initialization.
  Special case with std::array<dim_t, 0> to allow {}.
  Don't check shape rank; filldimv does it.
* test/big-0.cc: Give up on checks-of-failure, which only covered a few of the
  cases that fail.
* test/view-ops.cc: Test conversions between Views of different Dimv.
* test/ra-0.cc: Re-allow constructors of the pattern {{}, c} for 0-rank
  views. These had been broken in 062a1fcecd7f26c88e3ad5a8270448ab15e4fc34.

There's a performance regression in bench/bench-at bigs/small with gcc-14.3 on
one of my boxes, but not on the others, or on gcc-15.2. Not sure what that is
about.

TODO
@@ -138,7 +138,7 @@ Some of these aren't bugs in the sense that I expect to solve them, but more lik
       the error. On the other hand if RA_CHECK is 0, we shouldn't continue. But ofc we do in
       index checks etc. So is this different?
   17. [ ]
-  18. [ ]
+  18. [ ] Container can't maintain the invariant cp=store.data() because users can just change cp.
   19. [ ]
   20. [ ]
   21. [ ]

bench/bench-at.cc
@@ -47,8 +47,7 @@ int main(int argc, char * * argv)
                    bm.run([&]{
                        int val = 0;
                        for (int i=0; i<O; ++i) {
-// conversion needed for var rank I
-                           val += C(ra::dim_t(I(i, 0)), ra::dim_t(I(i, 1)));
+                           val += C(ra::dim_t(I(i, 0)), ra::dim_t(I(i, 1))); // need convert for var rank I
                        }
                        val0 = val;
                    }));

ra/arrays.hh
@@ -329,18 +329,18 @@ struct Container: public std::conditional_t<1==R,
 #define RA_BRACES(N) constexpr Container(braces<T, N> x) requires (R==ANY): Container(braces_shape<T, N>(x), x) {}
     RA_FE(RA_BRACES, 1, 2, 3, 4)
 #undef RA_BRACES
-    constexpr Container(auto && s, none_t)
+    constexpr Container(auto && s, auto const & x)
     {
         store = storage_traits<Store>::create(filldimv(iter(RA_FW(s)), dimv));
         cp = storage_traits<Store>::data(store);
+        if constexpr (!(requires { none_t(x); })) { view() = x; }
     }
-    constexpr Container(auto && s, auto const & x): Container(RA_FW(s), none) { view() = x; }
     constexpr Container(auto && s, std::initializer_list<T> x): Container(RA_FW(s), none) { to_ravel(x, *this); }
-// sharg overloads handle {...} arguments.
-    using sharg = decltype(shape(std::declval<Vu>()));
-    constexpr Container(sharg const & s, auto const & x): Container(iter(s), x) {}
-    constexpr Container(sharg const & s, std::initializer_list<T> x): Container(iter(s), x) {}
-    constexpr Container(sharg const & s, auto * p): Container(iter(s), none) { std::ranges::copy_n(p, size(), begin()); }
+    constexpr Container(std::array<dim_t, 0> s, auto const & x): Container(iter(s), x) {};
+    constexpr Container(std::array<dim_t, 0> s, std::initializer_list<T> x): Container(iter(s), x) {}
+    template <int N> constexpr Container(dim_t (&&s)[N], auto const & x): Container(iter(s), x) {}
+    template <int N> constexpr Container(dim_t (&&s)[N], std::initializer_list<T> x): Container(iter(s), x) {}
+    template <int N> constexpr Container(dim_t (&&s)[N], auto * p): Container(iter(s), none) { std::ranges::copy_n(p, size(), begin()); }
 // resize first axis or full shape. Only for some kinds of store.
     constexpr void resize(dim_t const s)
     {

test/big-0.cc
@@ -14,16 +14,29 @@
 
 using std::cout, std::endl, std::flush, ra::TestRecorder;
 
-template <class T> constexpr bool ctest1 = requires { ra::Big<T, 2> ({2, 3, 1}, 99); }; // bad shape for rank
-template <class T> constexpr bool ctest2 = requires { ra::Big<T, 2> ({2, 3, 1}, {1, 2, 3, 4, 5, 6}); }; // bad shape for rank
-template <class T> constexpr bool ctest3 = requires { ra::Big<T, 2> ({2, 3, 1}, ra::none); }; // bad shape for rank
-template <class T> constexpr bool ctest4 = requires { ra::Big<T, 0> ({3, 4}, 3.); }; // bad shape for rank
-
 struct test_type { int a; };
 
 int main(int argc, char * * argv)
 {
     TestRecorder tr;
+    tr.section("should-fail constructors [ra42]");
+    {
+// FIXME these errors depend on static_assert.
+        // ra::Big<int, 2> {3, 4}; // Invalid shape for rank
+        // ra::Big<int, 2> (2, ra::none); // shape arg must have rank 1 for array rank>1
+        // ra::Big<int, 2> {1, 2, 3, 4, 5, 6}; // bad deduced shape from content arg
+        // ra::Big<int, 2> (ra::Small<int, 3>{2, 3, 4}, 99.); // bad shape for rank
+// FIXME too many dims was requires-checkable when constructors took std::array<dim, N> for shape, but that allowed too few dims.
+        // ra::Big<int, 2> ({2, 3, 1}, 99); // too many dims
+        // ra::Big<int, 2> ({2, 3, 1}, {1, 2, 3, 4, 5, 6}); // too many dims
+        // ra::Big<int, 2> ({2, 3, 1}, ra::none); // too many dims
+        // ra::Big<int, 0> ({3, 4}, 3.); // too many dims
+// FIXME also on views
+        // ra::ViewBig<int *, 2> ({2}, nullptr); // too few dims
+        // ra::ViewBig<int *, 2> ({2, 3, 4}, nullptr); // too many dims
+        // ra::ViewBig<int *, 2> (4, nullptr); // bad rank (really too few dims)
+        // ra::ViewBig<int *, 1> (ra::Small<int, 1, 1>{{1}}, nullptr); // bad rank
+    }
     tr.section("predicates");
     {
         ra::ViewBig<int *, 2> a; // uninitialized
@@ -72,19 +85,6 @@ int main(int argc, char * * argv)
             tr.test_eq(3, a.len(0));
             tr.test_eq(2, a.len(1));
         }
-    }
-    tr.section("should-fail constructors");
-    {
-        static_assert(!ctest1<int>);
-        static_assert(!ctest2<int>);
-        static_assert(!ctest3<int>);
-        static_assert(!ctest4<int>);
-
-// FIXME these errors depend on static_assert so cannot be checked with requires.
-        // ra::Big<T, 2> {3, 4}; // Invalid shape for rank
-        // ra::Big<int, 2> (2, ra::none); // shape arg must have rank 1 for array rank>1
-        // ra::Big<T, 2> {1, 2, 3, 4, 5, 6}; // bad deduced shape from content arg
-        // ra::Big<T, 2> (ra::Small<int, 3>{2, 3, 4}, 99.); // bad shape for rank
     }
     tr.section("any rank 1 expression for the shape argument");
     {
@@ -263,11 +263,15 @@ int main(int argc, char * * argv)
         ra::Big<int, 1> b(4, ra::_0);
         tr.test_eq(ra::iota(4), b);
     }
+// size cannot be 0 so store must always be allocated.
     tr.section("default constructor for ct rank 0 Container");
     {
-        ra::Big<int, 0> a = {}; // uninitialized
-        a = 3;
-        tr.test_eq(3, a);
+        ra::Big<int, 0> b; // uninitialized
+        b = 3;
+        tr.test_eq(3, b);
+// FIXME on c++26 with constexpr new do
+        // constexpr int y = []{ ra::Big<int, 0> a; a = 3; return a(); }(); // uninitialized
+        // tr.test_eq(3, y);
     }
     tr.section("scalar constructor for rt rank 0 Container");
     {
@@ -301,6 +305,15 @@ int main(int argc, char * * argv)
         a = test_type { 3 };
         tr.test_eq(3, a[0].a);
         tr.test_eq(3, a[1].a);
+    }
+    tr.section("View from container");
+    {
+        ra::Big<int, 1> a({3}, 0);
+        auto b = a();
+        tr.info("new view").test(&a.cp!=&b.cp);
+// FIXME disallow access to Container::cp to preserve the invariant cp=store.data() [ra18]
+        auto const & c = a.view();
+        tr.info("same view").test(&a.cp==&c.cp);
     }
     return tr.summary();
 }